80cf185a9dbc2ee0695f0bb27a43b7649027e83a
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-01 15:01:41 -0700

    fixed bug now showing new tweet text on timeline

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -24,10 +24,11 @@
     [self getUserImage];
     
     self.tweetContentView.delegate = self;
-    
     self.tweetContentView.layer.borderColor = [[UIColor grayColor] CGColor];
     self.tweetContentView.layer.borderWidth = 1.0;
     self.tweetContentView.layer.cornerRadius = 8;
+    
+    self.characterCountLabel.text = @"280";
     
     self.profilePicView.layer.cornerRadius  = self.profilePicView.frame.size.width/2;
 
@@ -37,13 +38,18 @@
 
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
     // Set the max character limit
-    int characterLimit = 140;
+    int characterLimit = 280;
 
     // Construct what the new text would be if we allowed the user's latest edit
     NSString *newText = [self.tweetContentView.text stringByReplacingCharactersInRange:range withString:text];
 
     // TODO: Update character count label
-    self.characterCountLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)newText.length];
+    self.characterCountLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long) 280 - newText.length];
+    if (newText.length == 280) {
+        self.characterCountLabel.textColor = [UIColor redColor];
+    } else {
+        self.characterCountLabel.textColor = [UIColor blackColor];
+    }
 
     // Should the new text should be allowed? True/False
     return newText.length < characterLimit;
@@ -59,8 +65,9 @@
         if(error){
             NSLog(@"Error composing Tweet: %@", error.localizedDescription);
         } else{
-            [self dismissViewControllerAnimated:true completion:nil];
+            tweet.text = tweetText;
             [self.delegate didTweet:tweet];
+            [self dismissViewControllerAnimated:true completion:nil];
         }
     }];
 }
